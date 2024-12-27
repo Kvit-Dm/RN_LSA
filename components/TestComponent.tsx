bfcad4df030899ab666b109ee6b163d28ec8d440
Anti-RakPro <kvit.d.o@gmail.com>
2024-12-27 16:38:43 +0200

    fix:

---
+++ components/TestComponent.tsx
@@ -0,0 +1,13 @@
+import {Colors} from "react-native/Libraries/NewAppScreen";
+import {Text} from "react-native";
+import React from "react";
+
+function TestComponent() {
+    return(
+        <Text>
+            {'Welcome to TestComponent'}
+        </Text>
+    )
+}
+
+export default TestComponent;
